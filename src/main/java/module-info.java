e5bdefb40fbd02f7c570c818a7dcd4550b5c80dc
RyanWallace00 <n11280620@qut.edu.au>
2024-04-30 14:54:41 +1000

    Notifications PopUp Implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.cab302javaproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.datatransfer;
+    requires org.controlsfx.controls;
 
 
     opens com.example.cab302javaproject to javafx.fxml;
